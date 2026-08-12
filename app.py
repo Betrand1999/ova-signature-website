6c14aa0f90b33a65726104d1b3f566a6c0460c5d
BETRAND <mmutagha@gmail.com>
2026-08-12 17:17:41 -0400

    Add sitemap and robots.txt

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
@@ -13,6 +13,30 @@ def home():
 @app.route("/health")
 def health():
     return {"status": "healthy"}, 200
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+    xml = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://ovasignature.com/</loc>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+</urlset>
+"""
+    return Response(xml, mimetype="application/xml")
+
+
+@app.route("/robots.txt")
+def robots():
+    text = """User-agent: *
+Allow: /
+
+Sitemap: https://ovasignature.com/sitemap.xml
+"""
+    return Response(text, mimetype="text/plain")
 
 
 if __name__ == "__main__":
